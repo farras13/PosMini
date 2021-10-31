3f698e367e13faf4529df6e26a32cd2a7a7cb505
farras13 <farrasgopal13@gmail.com>
2021-10-31 10:21:09 +0700

    halaman Daftar product

---
+++ application/views/produk.php
@@ -0,0 +1,69 @@
+<section class="pt-4" style="font-size: smaller;">
+	<div class="container-fluid mx-1">
+			<p><b><h1>Produk</h1></b></p>
+		<!-- Page Features-->
+		<div class="row row-cols-1 row-cols-md-4 g-2 bor">
+			<div class="col">
+				<div class="card h-100 border-dark">
+					<img src="<?= base_url('assets/img/standard_repo.png') ?>" class="card-img-top" alt="...">
+					<div class="card-body">
+						<div class="card-title mb-4 mt-2">
+							<center>
+								<h5>majoo Pro</h5>
+								<h4><small>Rp</small><b> 25000</h4></b>
+							</center>
+						</div>
+						<p class="card-text"> Lorem ipsum is simply dummy text of the printing and typesetting industry.</p>
+					</div>
+					<center><button class="btn btn-outline-dark shadow mb-4 mt-2">Beli</button></center>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100 border-dark">
+					<img src="<?= base_url('assets/img/paket-advance.png') ?>" class="card-img-top" alt="...">
+					<div class="card-body">
+						<div class="card-title mb-4 mt-2">
+							<center>
+								<h5>majoo Advance</h5>
+								<h4><small>Rp</small><b> 25000</h4></b>
+							</center>
+						</div>
+						<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					</div>
+					<center><button class="btn btn-outline-dark shadow mb-4 mt-2">Beli</button></center>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100 border-dark">
+					<img src="<?= base_url('assets/img/paket-lifestyle.png') ?>" class="card-img-top" alt="...">
+					<div class="card-body">
+						<div class="card-title mb-4 mt-2">
+							<center>
+								<h5>majoo Lifestyle</h5>
+								<h4><small>Rp</small><b> 25000</h4></b>
+							</center>
+						</div>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					</div>
+					<center><button class="btn btn-outline-dark shadow mb-4 mt-2">Beli</button></center>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100 border-dark">
+					<img src="<?= base_url('assets/img/paket-desktop.png') ?>" class="card-img-top" alt="...">
+					<div class="card-body">
+						<div class="card-title mb-4 mt-2">
+							<center>
+								<h5>majoo Desktop</h5>
+								<h4><small>Rp</small><b> 25000</h4></b>
+							</center>
+						</div>
+						
+						<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt asperiores voluptatum eligendi. Mollitia, repellendus corporis dolore iure quaerat temporibus ratione esse nobis! Dolores ab quisquam tempore, ratione nam sunt aspernatur! </p>
+					</div>
+					<center><button class="btn btn-outline-dark shadow mb-4 mt-2">Beli</button></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
